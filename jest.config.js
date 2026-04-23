2852ef7801d23217e71af0675c1117be6f1b7101
Ivan Kalugin <i.am.hunter.777.ih@gmail.com>
2026-04-23 22:00:14 +0300

    feat(config): added jest config for testing

---
+++ jest.config.js
@@ -0,0 +1,21 @@
+module.exports = {
+  testEnvironment: 'node',
+  transform: {
+    '^.+\\.js$': 'babel-jest'
+  },
+  moduleNameMapper: {
+    '^@/(.*)$': '<rootDir>/src/$1'
+  },
+  testMatch: [
+    '**/tests/**/*.test.js'
+  ],
+  collectCoverage: true,
+  coverageDirectory: 'coverage',
+  coverageReporters: ['text', 'lcov', 'html'],
+  collectCoverageFrom: [
+    'src/**/*.js',
+    '!src/**/*.test.js'
+  ],
+  verbose: true,
+  testTimeout: 10000
+};
