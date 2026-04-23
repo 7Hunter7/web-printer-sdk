6f4e0a2afd53e1891e34eef3d2e8abe64b3c8cc9
Ivan Kalugin <i.am.hunter.777.ih@gmail.com>
2026-04-23 22:37:08 +0300

    feat(config): updated jest config

--- jest.config.js
+++ jest.config.js
@@ -9,12 +9,18 @@ module.exports = {
   testMatch: [
     '**/tests/**/*.test.js'
   ],
+  moduleDirectories: ['node_modules', 'src'],
+  rootDir: '.',
+  verbose: true,
   collectCoverage: true,
   coverageDirectory: 'coverage',
   coverageReporters: ['text', 'lcov', 'html'],
   collectCoverageFrom: [
     'src/**/*.js',
     '!src/**/*.test.js'
+  ],
+  transformIgnorePatterns: [
+    '/node_modules/'
   ],
   verbose: true,
   testTimeout: 10000
