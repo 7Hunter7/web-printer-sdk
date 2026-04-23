c85ae75d6eb202d92fb2f713daaa4a2ede7d5278
Ivan Kalugin <i.am.hunter.777.ih@gmail.com>
2026-04-23 20:39:38 +0300

    feat(printers): added  Virtual Printer for tests

---
+++ src/printers/VirtualPrinter.js
@@ -0,0 +1,72 @@
+// Для тестирования
+import BasePrinter from '../core/BasePrinter.js';
+import { PrinterError, ErrorCodes } from '../core/PrinterError.js';
+
+class VirtualPrinter extends BasePrinter {
+  constructor(config = {}) {
+    super(config);
+    this.printHistory = [];
+  }
+
+  async discover() {
+    return [
+      {
+        id: 'virtual-1',
+        name: 'Virtual Printer (Test)',
+        type: 'virtual',
+        description: 'Test printer that captures print data'
+      }
+    ];
+  }
+
+  async connect(config) {
+    this.isConnected = true;
+    this.config = { ...this.config, ...config };
+    return { success: true, message: 'Connected to virtual printer' };
+  }
+
+  async print(data) {
+    if (!this.isConnected) {
+      throw new PrinterError(ErrorCodes.NOT_CONNECTED, 'Virtual printer is not connected');
+    }
+
+    const printJob = {
+      timestamp: new Date().toISOString(),
+      data: data.toString(),
+      size: data.length,
+      type: Buffer.isBuffer(data) ? 'buffer' : 'string'
+    };
+    
+    this.printHistory.push(printJob);
+    
+    console.log('[VirtualPrinter] Print job received:', {
+      size: printJob.size,
+      preview: printJob.data.substring(0, 100) + (printJob.data.length > 100 ? '...' : '')
+    });
+    
+    return { 
+      success: true, 
+      message: 'Print job captured',
+      jobId: this.printHistory.length - 1,
+      preview: printJob.data.substring(0, 200)
+    };
+  }
+
+  async disconnect() {
+    this.isConnected = false;
+  }
+
+  getHistory() {
+    return [...this.printHistory];
+  }
+
+  clearHistory() {
+    this.printHistory = [];
+  }
+
+  getLastJob() {
+    return this.printHistory.length > 0 ? this.printHistory[this.printHistory.length - 1] : null;
+  }
+}
+
+export default VirtualPrinter;
