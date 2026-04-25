79e44374967a6e8e6f2821b9c844824e43019f5b
Ivan Kalugin <i.am.hunter.777.ih@gmail.com>
2026-04-25 07:31:29 +0300

    feat(VirtualPrinter): updated print()

--- src/printers/VirtualPrinter.js
+++ src/printers/VirtualPrinter.js
@@ -1,6 +1,6 @@
 // Для тестирования
-import BasePrinter from '../core/BasePrinter.js';
-import { PrinterError, ErrorCodes } from '../core/PrinterError.js';
+import BasePrinter from "../core/BasePrinter.js";
+import { PrinterError, ErrorCodes } from "../core/PrinterError.js";
 
 class VirtualPrinter extends BasePrinter {
   constructor(config = {}) {
@@ -11,44 +11,52 @@ class VirtualPrinter extends BasePrinter {
   async discover() {
     return [
       {
-        id: 'virtual-1',
-        name: 'Virtual Printer (Test)',
-        type: 'virtual',
-        description: 'Test printer that captures print data'
-      }
+        id: "virtual-1",
+        name: "Virtual Printer (Test)",
+        type: "virtual",
+        description: "Test printer that captures print data",
+      },
     ];
   }
 
   async connect(config) {
     this.isConnected = true;
     this.config = { ...this.config, ...config };
-    return { success: true, message: 'Connected to virtual printer' };
+    return { success: true, message: "Connected to virtual printer" };
   }
 
   async print(data) {
     if (!this.isConnected) {
-      throw new PrinterError(ErrorCodes.NOT_CONNECTED, 'Virtual printer is not connected');
+      throw new PrinterError(
+        ErrorCodes.NOT_CONNECTED,
+        "Virtual printer is not connected",
+      );
     }
+
+    const buffer = Buffer.isBuffer(data) ? data : Buffer.from(data);
 
     const printJob = {
       timestamp: new Date().toISOString(),
-      data: data.toString(),
-      size: data.length,
-      type: Buffer.isBuffer(data) ? 'buffer' : 'string'
+      data: buffer.toString(),
+      size: buffer.length,
+      type: Buffer.isBuffer(data) ? "buffer" : "string",
     };
-    
+
     this.printHistory.push(printJob);
-    
-    console.log('[VirtualPrinter] Print job received:', {
+
+    console.log("[VirtualPrinter] Print job received:", {
       size: printJob.size,
-      preview: printJob.data.substring(0, 100) + (printJob.data.length > 100 ? '...' : '')
+      preview:
+        printJob.data.substring(0, 100) +
+        (printJob.data.length > 100 ? "..." : ""),
     });
-    
-    return { 
-      success: true, 
-      message: 'Print job captured',
+
+    return {
+      success: true,
+      message: "Print job captured",
       jobId: this.printHistory.length - 1,
-      preview: printJob.data.substring(0, 200)
+      preview: printJob.data.substring(0, 200),
+      bytesWritten: buffer.length,
     };
   }
 
@@ -65,7 +73,9 @@ class VirtualPrinter extends BasePrinter {
   }
 
   getLastJob() {
-    return this.printHistory.length > 0 ? this.printHistory[this.printHistory.length - 1] : null;
+    return this.printHistory.length > 0
+      ? this.printHistory[this.printHistory.length - 1]
+      : null;
   }
 }
 
